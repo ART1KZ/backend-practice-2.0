0b9e847efdf0e77f77ab7d09328d87b03ac5f861
ART1KZ <artemkiselev18072k6@mail.ru>
2024-09-23 14:36:52 +0500

    update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,60 +1,67 @@
 import express from "express";
 
-const app = express();
-const port = process.env.PORT || 3000;
+const app = new express();
+const port = 3000;
 
 const db = {
-  roles: [
-    {
-      id: 1,
-      name: "admin",
-    },
-    {
-      id: 2,
-      name: "user",
-    },
-    {
-      id: 3,
-      name: "moderator",
-    },
-  ],
-  users: [
-    {
-      id: 1,
-      name: "Artem",
-      age: 18,
-    },
-    {
-      id: 2,
-      name: "Dmitry",
-      age: 19,
-    },
-    {
-      id: 3,
-      name: "Alexander",
-      age: 21,
-    },
-  ],
   cities: [
-    {
-      id: 1,
-      name: "Surgut",
-    },
-    {
-      id: 2,
-      name: "Moscow",
-    },
-    {
-      id: 3,
-      name: "Saint-Petersburg",
-    },
+    { id: 1, name: "Surgut" },
+    { id: 2, name: "Moscow" },
+    { id: 3, name: "Kazan" },
   ],
 };
 
-app.listen(port, () => console.log("Приложение запущено по порту " + port));
+app.listen(port, () => {
+  console.log(`Начал прослушивать ${port} порт.`);
+}); // запуск приложения
 
 app.get("/", (req, res) => res.send("GET main page"));
-app.get("/api", (req, res) => res.json(db));
-app.get("/api/roles", (req, res) => {res.json(db.roles)});
-app.get("/api/users", (req, res) => {res.json(db.users)});
-app.get("/api/cities", (req, res) => {res.json(db.cities)});
+
+app.get("/cities", (req, res) => {
+  if (req.query.name) {
+    // проверка на наличие query параметра name
+    const searchString = req.query.name.toString(); // присвоение переменной значения query параметра name (строка)
+    const findedCity = db.cities.filter(
+      (obj) => obj.name.indexOf(searchString) > -1
+    ); // поиск города по query параметру name
+    if (findedCity.length !== 0) {
+      // проверка, найден ли город по query параметру
+      res.status(200).json(findedCity);
+    }
+    res.sendStatus(404);
+  } else if (req.query.id) {
+    // проверка на наличие query параметра id
+    const findedCity = db.cities.filter((obj) => obj.id === +req.query.id);
+    if (findedCity.length !== 0) {
+      // проверка, найден ли город по query параметру
+      res.status(200).json(findedCity);
+    }
+    res.sendStatus(404);
+  }
+
+  res.send(db.cities); // передаем в ответ все города, если нет нужных query параметров
+}); // запрос на получение всех городов или конкретного города по query параметрам
+
+app.get("/cities/:id", (req, res) => {
+  const findCity = db.cities.find((v) => {
+    // находим город по id, переданному в uri параметр
+    return v.id === +req.params.id;
+  });
+  if (findCity) {
+    // проверка, найден ли город
+    res.send(findCity); // передаем в ответ найденный город
+  } else {
+    res.send(404); // выдаем ошибку, если город не найден
+  }
+}); // запрос на получение города по id, переданному в uri параметре
+
+app.delete("/cities/:id", (req, res) => {
+  const findCity = db.cities.filter((obj) => obj.id === +req.params.id)[0]; // поиск города по id, переданному в uri параметре
+
+  db.cities = db.cities.filter(
+    (obj) => JSON.stringify(obj) !== JSON.stringify(findCity) // удаляем найденный по id город из массива cities
+  );
+
+  res.json(findCity); // передаем в ответ удаленный город
+  console.log(db.cities); // вывод в консоль текущего наполнения массива с городами
+}); // запрос на удаление города по переданному id в uri параметрe
