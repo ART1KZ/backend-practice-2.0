28ac0d1d00480d3d4cdac3b019eaab61ed0684a1
ART1KZ <artemkiselev18072k6@mail.ru>
2024-09-23 17:49:05 +0500

    added get, delete, post, put routes

--- src/index.ts
+++ src/index.ts
@@ -1,67 +1,86 @@
 import express from "express";
+import bodyParser from "body-parser";
 
 const app = new express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+const parserMiddleware = bodyParser();
 
 const db = {
   cities: [
     { id: 1, name: "Surgut" },
     { id: 2, name: "Moscow" },
     { id: 3, name: "Kazan" },
+    { id: 4, name: "Tyumen" },
+    { id: 5, name: "Novosibirsk" },
   ],
 };
 
+app.use(parserMiddleware);
+
 app.listen(port, () => {
   console.log(`Начал прослушивать ${port} порт.`);
-}); // запуск приложения
+});
 
-app.get("/", (req, res) => res.send("GET main page"));
+app.get("/", (req: Request, res: Response) => res.send("GET main page"));
 
-app.get("/cities", (req, res) => {
+app.get("/cities", (req: Request, res: Response) => {
   if (req.query.name) {
-    // проверка на наличие query параметра name
-    const searchString = req.query.name.toString(); // присвоение переменной значения query параметра name (строка)
-    const findedCity = db.cities.filter(
-      (obj) => obj.name.indexOf(searchString) > -1
-    ); // поиск города по query параметру name
-    if (findedCity.length !== 0) {
-      // проверка, найден ли город по query параметру
-      res.status(200).json(findedCity);
-    }
-    res.sendStatus(404);
+    const searchString = req.query.name.toString();
+    const foundCities = db.cities.filter((obj) =>
+      obj.name.includes(searchString)
+    );
+    return foundCities.length === 0
+      ? res.status(404).json({ error: "По указанному name городов не найдено" })
+      : res.json(foundCities);
   } else if (req.query.id) {
-    // проверка на наличие query параметра id
-    const findedCity = db.cities.filter((obj) => obj.id === +req.query.id);
-    if (findedCity.length !== 0) {
-      // проверка, найден ли город по query параметру
-      res.status(200).json(findedCity);
-    }
-    res.sendStatus(404);
+    const foundCity = db.cities.find((obj) => obj.id === +req.query.id);
+    return foundCity
+      ? res.json(foundCity)
+      : res.status(404).json({ error: "По указанному id городов не найдено" });
+  } else if (JSON.stringify(req.query) !== "{}") {
+    return res
+      .status(404)
+      .json({ error: "Некорректные query-параметры запроса" });
   }
+  return res.status(200).json(db.cities);
+});
 
-  res.send(db.cities); // передаем в ответ все города, если нет нужных query параметров
-}); // запрос на получение всех городов или конкретного города по query параметрам
+app.get("/cities/:id", (req: Request, res: Response) => {
+  const foundCity = db.cities.find((obj) => obj.id === +req.params.id);
+  return foundCity
+    ? res.status(200).json(foundCity)
+    : res.status(404).json({ error: "Некорректный URI параметр запроса" });
+});
 
-app.get("/cities/:id", (req, res) => {
-  const findCity = db.cities.find((v) => {
-    // находим город по id, переданному в uri параметр
-    return v.id === +req.params.id;
-  });
-  if (findCity) {
-    // проверка, найден ли город
-    res.send(findCity); // передаем в ответ найденный город
-  } else {
-    res.send(404); // выдаем ошибку, если город не найден
+app.delete("/cities/:id", (req: Request, res: Response) => {
+  const foundCity = db.cities.find((obj) => obj.id === +req.params.id);
+  if (foundCity) {
+    db.cities = db.cities.filter(
+      (obj) => obj.id !== +req.params.id
+    );
+    console.log(db.cities)
+    return res.sendStatus(204);
   }
-}); // запрос на получение города по id, переданному в uri параметре
+  return res.status(404).json({ error: "Города с таким id не существует"});
+});
 
-app.delete("/cities/:id", (req, res) => {
-  const findCity = db.cities.filter((obj) => obj.id === +req.params.id)[0]; // поиск города по id, переданному в uri параметре
+app.delete("/cities", (req: Request, res: Response) => res.status(404).json({ error: "Я запрещаю вам удалять все города"}))
 
-  db.cities = db.cities.filter(
-    (obj) => JSON.stringify(obj) !== JSON.stringify(findCity) // удаляем найденный по id город из массива cities
-  );
+app.post("/cities", (req: Request, res: Response) => {
+  const newCity = {
+    id: +(new Date()),
+    name: req.body.name
+  };
+  db.cities.push(newCity);
+  return res.status(201).json(newCity)
+})
 
-  res.json(findCity); // передаем в ответ удаленный город
-  console.log(db.cities); // вывод в консоль текущего наполнения массива с городами
-}); // запрос на удаление города по переданному id в uri параметрe
+app.put("/cities/:id", (req: Request, res: Response) => {
+  const foundCity = db.cities.find((obj) => obj.id === +req.params.id);
+  if(foundCity) {
+    foundCity.name = req.body.name;
+    console.log(db.cities)
+    return res.status(201).json(foundCity)
+  }
+  return res.status(404).json({ error: "Города с указанным id не найдено"}) 
+})
